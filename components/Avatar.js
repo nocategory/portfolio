2af3ad475eb137db290514f8e6b71017a67f7480
nocategory <nocategory@tuta.io>
2020-08-06 19:40:20 +0100

    Add avatar to home

---
+++ components/Avatar.js
@@ -0,0 +1,13 @@
+const Avatar = () => (
+  <>
+    <img src="/nocat.svg" />
+    <style jsx>{`
+      img {
+        width: 400px;
+        height: 400px;
+      }
+    `}</style>
+  </>
+)
+
+export default Avatar
--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,58 @@
 import Head from 'next/head'
+import Avatar from '@components/Avatar'
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>nocategory's portfolio</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const Home = () => (
+  <div className="container">
+    <Head>
+      <title>nocategory's portfolio</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-      <main></main>
+    <main>
+      <Avatar />
+    </main>
 
-      <style jsx>{`
-        .container {
-          height: 100vh;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          background-color: rgb(29, 29, 32);
-        }
+    <style jsx>{`
+      .container {
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background-color: #201f24;
+      }
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+      main {
+        padding: 5rem 0;
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
 
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-family: Menlo, Monaco, Lucida Console, Courier New, monospace;
-        }
-      `}</style>
+      code {
+        background: #fafafa;
+        border-radius: 5px;
+        padding: 0.75rem;
+        font-family: Menlo, Monaco, Lucida Console, Courier New, monospace;
+      }
+    `}</style>
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
+    <style jsx global>{`
+      html,
+      body {
+        padding: 0;
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+      }
 
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
-}
+      * {
+        box-sizing: border-box;
+      }
+    `}</style>
+  </div>
+)
+
+export default Home
