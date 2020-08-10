9efa72b95d933f5d27ae7d699d938798fbfed5e2
nocategory <nocategory@tuta.io>
2020-08-10 22:45:14 +0100

    Add prop for font color in Layout component

--- components/Layout.js
+++ components/Layout.js
@@ -17,7 +17,7 @@ const fadeMenu = {
   },
 }
 
-const Layout = ({ children, bgColor }) => (
+const Layout = ({ children, bgColor, theme }) => (
   <motion.div
     className="container"
     style={{ backgroundColor: bgColor }}
@@ -29,6 +29,7 @@ const Layout = ({ children, bgColor }) => (
     <Head>
       <title>nocategory's portfolio</title>
       <link rel="icon" href="/favicon.ico" /> {/* TODO: change the favicon */}
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
     <main>{children}</main>
 
@@ -40,25 +41,19 @@ const Layout = ({ children, bgColor }) => (
         justify-content: center;
         align-items: center;
       }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-family: Menlo, Monaco, Lucida Console, Courier New, monospace;
-      }
     `}</style>
 
     <style jsx global>{`
       html,
       body {
-        color: rgba(255, 255, 255, 0.8);
-        background-color: ${bgColor}
+        color: ${theme === 'dark' ? '#dadada' : '#201f24'};
+        background-color: ${bgColor};
         padding: 0;
         margin: 0;
         font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
           Helvetica, Arial, sans-serif, Ubuntu, 'Apple Color Emoji',
           'Segoe UI Emoji', 'Segoe UI Symbol';
+        line-height: 1.5;
       }
 
       * {
