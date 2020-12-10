b612463555f286e3b5fc9fa6a9e0278d7268e74d
nocategory <me@nocategory.dev>
2020-12-10 21:43:59 +0000

    Add new favicon & update head

--- components/Layout.js
+++ components/Layout.js
@@ -30,7 +30,18 @@ const Layout = ({ children, bgColor, theme }) => (
   >
     <Head>
       <title>nocategory's portfolio</title>
-      <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
     <main>{children}</main>
