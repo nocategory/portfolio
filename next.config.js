dd065370f0c8a989f1aac5c16329a904e20cbff2
nocategory <me@nocategory.dev>
2020-12-22 20:02:10 +0000

    Remove next-compose-plugins dependency

--- next.config.js
+++ next.config.js
@@ -1,22 +1,16 @@
-const withPlugins = require('next-compose-plugins')
 const optimizedImages = require('next-optimized-images')
 const path = require('path')
 
-module.exports = withPlugins([
-  [
-    optimizedImages,
-    {
-      target: 'serverless',
-      webpack(config) {
-        config.resolve.alias.images = path.join(__dirname, 'images')
-        return config
-      },
-      images: {
-        // for example
-        handleImages: ['jpeg', 'png', 'svg', 'webp', 'gif', 'ico'],
-      },
-      optimize: true,
-      limit: 1,
-    },
-  ],
-])
+module.exports = optimizedImages({
+  target: 'serverless',
+  webpack(config) {
+    config.resolve.alias.images = path.join(__dirname, 'images')
+    return config
+  },
+  images: {
+    // for example
+    handleImages: ['jpeg', 'png', 'svg', 'webp', 'gif', 'ico'],
+  },
+  optimize: true,
+  limit: 1,
+})
