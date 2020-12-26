264608d391dff25506558effdfde24b778da9da6
nocategory <me@nocategory.dev>
2020-12-26 23:30:14 +0000

    Fix images not showing on Netlify

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+const path = require('path')
+
 module.exports = {
   target: 'serverless',
+  webpack(config) {
+    config.resolve.alias.images = path.join(__dirname, 'public/images')
+    return config
+  },
 }
