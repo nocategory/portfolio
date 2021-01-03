d592cdf53f1191056ddb5afde6961fd2b2abb061
nocategory <me@nocategory.dev>
2021-01-03 19:09:33 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,3 @@
 module.exports = {
-  target: 'serverless',
-  i18n: {
-    locales: ['en-US'],
-    defaultLocale: 'en-US',
-  },
-  async headers() {
-    return [
-      {
-        source: '/oss',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 's-maxage=86400',
-          },
-        ],
-      },
-    ]
-  },
+  target: 'serverless'
 }
